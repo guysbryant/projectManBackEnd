8160b7771b32ef64ff88d8096c2ca49e58ceb9de
guysbryant <guy.s.bryant@gmail.com>
2021-05-10 21:16:44 -0400

    revert user serializer to just name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-  has_many :users_tasks
-  has_many :tasks, through: :users_tasks
-  has_many :projects, through: :tasks
 end
