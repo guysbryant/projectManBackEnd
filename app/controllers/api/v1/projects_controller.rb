5e7c64eaf237fe06715abb018f38d34f3e5fce15
guysbryant <guy.s.bryant@gmail.com>
2021-05-12 00:55:14 -0400

    add create route and strong params

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::ProjectsController < ApplicationController
     projects = Project.all
     render json: ProjectSerializer.new(projects)
   end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      render json: ProjectSerializer.new(project), status: :accepted
+    else
+      render json: {errors: project.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
